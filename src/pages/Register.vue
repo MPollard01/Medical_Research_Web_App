4c44fb7f08c45cf39349c7c2527cf5abcbd76942
Joeharwood96 <joeharwood3@gmail.com>
2021-02-05 16:21:17 +0000

    initial commit for home and register pages

---
+++ src/pages/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"
+    />
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+    />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      console.log(this.email, this.password)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
