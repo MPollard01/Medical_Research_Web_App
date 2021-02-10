4cfc1d230bdf6dd6eee3b2270079af75ecac00e8
Cameron <cam.lander999@gmail.com>
2021-02-10 18:05:05 +0000

    Changed router history mode
    Changed router history mode to stop hashtags from appearing in URLs.

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,38 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/pages/Home'
-import Login from '@/pages/Login'
-import Register from '@/pages/Register'
-import RegisterConfirmation from '@/pages/RegisterConfirmation'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import RegisterConfirmation from "@/pages/RegisterConfirmation";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: Register
-    },
-    {
-      path: '/register-confirmation',
-      name: 'RegisterConfrimation',
-      component: RegisterConfirmation
-    }
-  ]
-})
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register
+  },
+  {
+    path: "/register-confirmation",
+    name: "RegisterConfrimation",
+    component: RegisterConfirmation
+  }
+];
+
+const router = new Router({
+  mode: "history",
+  routes
+});
+
+export default router;
